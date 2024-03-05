539e8c6272e0555cadb9214af32f026f46c03430
s.hirose <shunichi_hirose@pj100.biz>
2024-03-05 17:42:58 +0900

    👍 add launch,json

--- application/src/main.ts
+++ application/src/main.ts
@@ -4,13 +4,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
-    .setTitle('Todo API Project')
-    .setDescription('Todo API description')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  if (process.env.NODE_ENV === 'development') {
+    const config = new DocumentBuilder()
+      .setTitle('Todo API Project')
+      .setDescription('Todo API description')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/docs', app, document);
+  }
   await app.listen(8000);
 }
 bootstrap();
